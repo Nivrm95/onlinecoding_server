3c01571cc3f02e0b4ea1140eca73814bcf9db659
Niv Rahamim <nivrm@gmail.com>
2023-04-21 23:38:02 +0300

    Cors

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ connectToDB();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://nivprojectclient.onrender.com"],
     methods: ["GET", "POST"],
   },
 });
