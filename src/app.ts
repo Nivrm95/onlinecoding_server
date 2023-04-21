c2426cc9d34df98aee92c2d353ab83da27bf2afd
Niv Rahamim <nivrm@gmail.com>
2023-04-21 22:59:23 +0300

    finish

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,3 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import cors from "cors";
-// import router from "./index";
-// import { connectToDB } from "./connection";
-// require("dotenv").config();
-
-// const app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(cors());
-// app.use(router);
-// connectToDB();
-// app.listen(process.env.PORT, () =>
-//   console.log(`Listening on http://localhost:${process.env.PORT}`)
-// );
-
-//////////socket/////////////
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -41,16 +23,24 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log(`User Connected: ${socket.id}`);
   const roomId: string = socket.handshake.query.roomId as string;
-  socket.join(roomId);
+  await socket.join(roomId);
   console.log(io.sockets.adapter.rooms);
-  
- 
+
+  socket.to(roomId).emit("connected", {
+    roomSize: io.sockets.adapter.rooms.get(roomId)?.size,
+  });
+
+  socket.emit("roomSize", {
+    roomSize: io.sockets.adapter.rooms.get(roomId)?.size,
+  });
+
   socket.on("code", ({ code }) => {
     console.log(`Received code from ${socket.id}`);
     socket.to(roomId).emit("code", { code, senderId: socket.id });
+    console.log();
   });
 
   socket.on("disconnect", () => {
